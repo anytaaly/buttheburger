99968a8ea4c738cfc3e9b5558890e3c5aade197e
Anyta Ali <anyta.ali86@gmail.com>
2018-04-04 09:36:18 -0500

    updated

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Seed Burgers into Database ... 
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Chilli Cheese Burger", false, "2016-11-05 04:20:00"),
+  ("Big Mac", true, "2016-11-05 04:20:01"),
+  ("Cheesburger", false, "2016-11-05 04:20:02"),
+  ("QuarterPonder", false, "2016-11-05 16:20:00")
---
+++ routes/burgers_controllers.js
@@ -0,0 +1,4 @@
+// Node Dependencies
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js');
